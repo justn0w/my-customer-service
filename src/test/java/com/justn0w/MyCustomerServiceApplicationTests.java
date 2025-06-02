f92447949568f1065e0e0aa2cbef01677a8d5a29
justn0w <ggjustnow@163.com>
2025-06-02 18:21:16 +0800

    first commit

---
+++ src/test/java/com/justn0w/MyCustomerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.justn0w;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyCustomerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
